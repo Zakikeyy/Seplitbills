191b8e4c15bfcf8a9d811237c35f8f9cc6f7bae0
zakikeyy <zakidewasiwa95@gmail.com>
2025-08-12 23:13:43 +0700

    Perbaiki path Template

--- api/index.py
+++ api/index.py
@@ -1,8 +1,14 @@
 # app.py
 
+import os
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+# Tentukan path absolut ke direktori proyek
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+template_folder = os.path.join(project_root, 'templates')
+static_folder = os.path.join(project_root, 'static')
+
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
 
 @app.route('/')
 def halaman_input():
